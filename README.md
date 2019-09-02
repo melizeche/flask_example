590fba30f13e6be9df8d1d6c221f1067cf097e32
Marcelo Elizeche Landó <melizeche@gmail.com>
2019-09-02 16:36:42 +0200

    Update README with a flask example, add app.py and templates

--- README.md
+++ README.md
@@ -19,3 +19,11 @@
  ## Run the example
  `flask run`
  open the browser in http://localhost:5000/
+
+ ## Make another view
+```
+@app.route("/page2wow")
+def view2():
+    htmlcontent="<h1>Hi I'm a header</h1><p>I'm a paragraph</p>"
+    return htmlcontent
+```
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!!!!!"
+
+@app.route("/page2wow")
+def view2():
+    htmlcontent="<h1>Hi I'm a header</h1><p>I'm a paragraph</p>"
+    return htmlcontent
+
+@app.route("/2")
+def view_template():
+    return render_template("index.html")
+
+@app.route("/3")
+def pass_arguments():
+    my_name = "Marcelo"
+    username = request.args.get('username')
+    return render_template("three.html", name=my_name, surname="Elizeche", username=username)
+
